b25583d286ededbebf71b6cdfb0588347b52ef36
Quan Hui Wang <quanhui.wang@d2l.com>
2020-02-19 17:12:40 -0500

    Move uploadFile function to be declared in the constructor.

--- src/state/uploader.js
+++ src/state/uploader.js
@@ -6,40 +6,39 @@ export class Uploader {
 	constructor({ apiClient }) {
 		this.uploads = [];
 		this.apiClient = apiClient;
+		this.uploadFile = flow(function * (file) {
+			/* eslint-disable no-invalid-this */
+			this.uploads.push({ file, progress: 0 });
+			const content = yield this.apiClient.createContent();
+			const revision = yield this.apiClient.createRevision(content.id, {
+				type: 'Scorm',
+				title: file.name,
+				extension: file.extension
+			});
+			const uploader = new S3Uploader({
+				file,
+				key: revision.s3Key,
+				signRequest: ({ file, key }) => this.apiClient.signUploadRequest({
+					fileName: key,
+					contentType: file.type,
+					contentDisposition: 'auto'
+				}),
+				onProgress: progress => {
+					const upload = this.uploads.find(upload => upload.file === file);
+					if (upload) {
+						upload.progress = progress;
+					}
+				}
+			});
+			yield uploader.upload();
+			this.uploads = this.uploads.filter(upload => upload.file !== file);
+			/* eslint-enable no-invalid-this */
+		});
 	}
 
 	getUploads() {
 		return this.uploads;
 	}
-
-	uploadFile = flow(function * (file) {
-		/* eslint-disable no-invalid-this */
-		this.uploads.push({ file, progress: 0 });
-		const content = yield this.apiClient.createContent();
-		const revision = yield this.apiClient.createRevision(content.id, {
-			type: 'Scorm',
-			title: file.name,
-			extension: file.extension
-		});
-		const uploader = new S3Uploader({
-			file,
-			key: revision.s3Key,
-			signRequest: ({ file, key }) => this.apiClient.signUploadRequest({
-				fileName: key,
-				contentType: file.type,
-				contentDisposition: 'auto'
-			}),
-			onProgress: progress => {
-				const upload = this.uploads.find(upload => upload.file === file);
-				if (upload) {
-					upload.progress = progress;
-				}
-			}
-		});
-		yield uploader.upload();
-		this.uploads = this.uploads.filter(upload => upload.file !== file);
-		/* eslint-enable no-invalid-this */
-	})
 }
 
 decorate(Uploader, {
