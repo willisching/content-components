dc57eb813a24a6a0027682f338f224cb09739b88
AdmiralCoco <dawingc@hotmail.com>
2020-09-16 11:53:27 -0400

    Comment out mocked up pages

--- src/pages/admin/d2l-capture-central-admin.js
+++ src/pages/admin/d2l-capture-central-admin.js
@@ -25,12 +25,11 @@ class D2lCaptureCentralAdmin extends DependencyRequester(PageViewElement) {
 			}
 
 			.d2l-capture-central-sidebar {
-				padding-top: 20px;
 				display: grid;
-				grid-template-rows: 30px;
-				grid-auto-rows: 20px;
+				grid-auto-rows: 25px;
 				grid-row-gap: 30px;
-				overflow-y: auto;
+				grid-template-rows: 30px;
+				padding-top: 20px;
 			}
 
 			.d2l-capture-central-server-options {
@@ -55,14 +54,15 @@ class D2lCaptureCentralAdmin extends DependencyRequester(PageViewElement) {
 			<div class="d2l-capture-central-admin">
 				<div class="d2l-capture-central-sidebar">
 					<h3 class="d2l-heading-3">${this.localize('captureCentral')}</h3>
-					<span><d2l-link @click=${this._goTo('/live-events')}>${this.localize('liveEvents')}</d2l-link></span>
+					<!-- <span><d2l-link @click=${this._goTo('/live-events')}>${this.localize('liveEvents')}</d2l-link></span> -->
 					<span><d2l-link @click=${this._goTo('/presentations')}>${this.localize('presentations')}</d2l-link></span>
-					<span><d2l-link @click=${this._goTo('/folders')}>${this.localize('folders')}</d2l-link></span>
+					<!-- <span><d2l-link @click=${this._goTo('/folders')}>${this.localize('folders')}</d2l-link></span> -->
 					<span><d2l-link @click=${this._goTo('/upload-video')}>${this.localize('uploadVideo')}</d2l-link></span>
-					<span><d2l-link>${this.localize('recordVideo')}</d2l-link></span>
+					<!-- <span><d2l-link>${this.localize('recordVideo')}</d2l-link></span> -->
 					<span><d2l-link @click=${this._goTo('/course-videos')}>${this.localize('courseVideos')}</d2l-link></span>
 				</div>
-				<div class="d2l-capture-central-server-options">
+				<!-- Mocked up pages commented out for now -->
+				<!-- <div class="d2l-capture-central-server-options">
 					<h3 class="d2l-heading-3">${this.localize('server')}</h3>
 					<div class="d2l-capture-central-grouping">
 						<h4 class="d2l-heading-4">${this.localize('prepostRollHeading')}</h4>
@@ -75,7 +75,7 @@ class D2lCaptureCentralAdmin extends DependencyRequester(PageViewElement) {
 						<d2l-link @click=${this._goTo('/live-events-reporting')}>${this.localize('liveEvents')}</d2l-link>
 						<d2l-link @click=${this._goTo('/visits')}>${this.localize('visits')}</d2l-link>
 					</div>
-				</div>
+				</div> -->
 			</div>
 		`;
 	}
