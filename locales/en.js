3a57802a98d16b088c002e2c0fe83d4f7a1fc9af
AdmiralCoco <dawingc@hotmail.com>
2020-10-01 14:44:23 -0400

    Add dummy locale files

--- locales/en.js
+++ locales/en.js
@@ -1,5 +1,11 @@
 export const val = {
-	seek: 'Seek',
-	mark: 'mark',
+	addNewChapter: 'Add new chapter',
+	chapterTitle: 'Chapter title',
 	cut: 'Cut',
+	delete: 'Delete',
+	mark: 'mark',
+	noItems: 'There are no items for this archive.',
+	seek: 'Seek',
+	tableOfContents: 'Table of Contents',
+	timeToSeekPosition: 'Set time to seek position',
 };
--- src/internal-localize-mixin.js
+++ src/internal-localize-mixin.js
@@ -9,8 +9,53 @@ const internalLocalizeMixin = superClass => class extends LocalizeMixin(superCla
 			let translations;
 			switch (lang) {
 				/* eslint-disable no-await-in-loop */
+				case 'ar':
+					translations = await import('../../locales/ar.js');
+					break;
+				case 'da-dk':
+					translations = await import('../../locales/da-dk.js');
+					break;
+				case 'de':
+					translations = await import('../../locales/de.js');
+					break;
 				case 'en':
 					translations = await import('../../locales/en.js');
+					break;
+				case 'es':
+					translations = await import('../../locales/es.js');
+					break;
+				case 'fi':
+					translations = await import('../../locales/fi.js');
+					break;
+				case 'fr':
+					translations = await import('../../locales/fr.js');
+					break;
+				case 'fr-fr':
+					translations = await import('../../locales/fr-fr.js');
+					break;
+				case 'ja':
+					translations = await import('../../locales/ja.js');
+					break;
+				case 'ko':
+					translations = await import('../../locales/ko.js');
+					break;
+				case 'nl':
+					translations = await import('../../locales/nl.js');
+					break;
+				case 'pt':
+					translations = await import('../../locales/pt.js');
+					break;
+				case 'sv':
+					translations = await import('../../locales/sv.js');
+					break;
+				case 'tr':
+					translations = await import('../../locales/tr.js');
+					break;
+				case 'zh-tw':
+					translations = await import('../../locales/zh-tw.js');
+					break;
+				case 'zh':
+					translations = await import('../../locales/zh.js');
 					break;
 				default:
 					continue;
