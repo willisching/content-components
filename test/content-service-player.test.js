8837353ab92731f2e8a9fdd5f318eee7f1a78b88
Russell Schick <rschick@bellwoods.ca>
2020-11-17 09:20:30 -0800

    Proof of concept

--- test/content-service-player.test.js
+++ test/content-service-player.test.js
@@ -1,19 +1,19 @@
-import '../<%= shortName %>.js';
+import '../content-service-viewer.js';
 import { expect, fixture, html } from '@open-wc/testing';
 import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
 
-describe('<%= name %>', () => {
+describe('d2l-labs-content-service-viewer', () => {
 
 	describe('accessibility', () => {
 		it('should pass all axe tests', async() => {
-			const el = await fixture(html`<<%= name %>></<%= name %>>`);
+			const el = await fixture(html`<d2l-labs-content-service-viewer></d2l-labs-content-service-viewer>`);
 			await expect(el).to.be.accessible();
 		});
 	});
 
 	describe('constructor', () => {
 		it('should construct', () => {
-			runConstructor('<%= name %>');
+			runConstructor('d2l-labs-content-service-viewer');
 		});
 	});
 
