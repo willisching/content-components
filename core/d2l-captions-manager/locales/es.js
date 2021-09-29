0bf6cf507bec6e09d591dd68c8ff2a9cc26ef1db
JenkinsReader <resupport@desire2learn.com>
2021-09-29 16:08:52 -0400

    Translation Updates

---
+++ core/d2l-captions-manager/locales/es.js
@@ -0,0 +1,18 @@
+export const val = {
+	addCaptions: 'Agregar subtítulos',
+	browseForCaptionsFile: 'Busque un archivo de subtítulos en su dispositivo.',
+	captionsFile: 'Archivo de subtítulos',
+	captionsLanguage: 'Idioma de subtítulos',
+	clickHereToReload: 'Haga clic aquí para intentar volver a cargar los subtítulos.',
+	deletingFile: 'Eliminando {filename}…',
+	failedToLoadCaptions: 'No se pudieron cargar los subtítulos para este tema.',
+	loading: 'Cargando…',
+	manageCaptions: 'Administrar subtítulos',
+	noCaptionsForTopic: 'Actualmente no hay subtítulos para este tema.',
+	removeCaptionsForLanguage: 'Eliminar subtítulos para el idioma {langName}.',
+	selectALanguage: 'Seleccione un idioma…',
+	selectLanguageForNewCaptions: 'Seleccione un idioma para los subtítulos que se agregarán. Después de seleccionar, a continuación aparecerá un botón con la opción “Examinar” para seleccionar el archivo de subtítulos.',
+	supportedTypesAndRenameConvention: 'Se admiten los formatos de subtítulos SRT y WebVTT. El archivo de subtítulos se guardará automáticamente después de seleccionarlo. El archivo de subtítulo será renombrado con la siguiente convención de nomenclatura: <videofilename>.<language>.<filetype>',
+	uploadingFile: 'Cargando {filename}…',
+	useControlsToAddCaptions: 'Utilice los controles a continuación para cargar un archivo de subtítulos.',
+};
---
+++ core/d2l-captions-manager/locales/ko.js
@@ -0,0 +1,18 @@
+export const val = {
+	addCaptions: '캡션 추가',
+	browseForCaptionsFile: '장치에서 캡션 파일을 찾습니다.',
+	captionsFile: '캡션 파일',
+	captionsLanguage: '캡션 언어',
+	clickHereToReload: '캡션을 다시 로드하려면 여기를 클릭하십시오.',
+	deletingFile: '{filename} 삭제 중...',
+	failedToLoadCaptions: '이 항목에 대한 캡션을 로드하지 못했습니다.',
+	loading: '로드 중...',
+	manageCaptions: '캡션 관리',
+	noCaptionsForTopic: '현재 이 주제에 대한 캡션이 없습니다.',
+	removeCaptionsForLanguage: '언어 {langName}에 대한 캡션을 제거합니다.',
+	selectALanguage: '언어 선택...',
+	selectLanguageForNewCaptions: '추가할 캡션의 언어를 선택합니다. 선택 후 캡션 파일을 선택할 수 있는 찾아보기 버튼이 아래에 나타납니다.',
+	supportedTypesAndRenameConvention: 'SRT 및 WebVTT 캡션 형식을 지원합니다. 캡션 파일을 선택하면 자동으로 저장됩니다. 다음 명명 규칙을 사용하여 이름을 바꿉니다. <videofilename>.<language>.<filetype>',
+	uploadingFile: '{filename} 업로드 중...',
+	useControlsToAddCaptions: '아래 컨트롤을 사용하여 캡션 파일을 업로드합니다.',
+};
