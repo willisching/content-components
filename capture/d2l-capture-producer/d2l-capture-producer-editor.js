4c920c7221cc20de8d8e12916f5df5762f24ef56
Mark Padilla <markjjpadilla@gmail.com>
2021-11-03 16:26:14 -0400

    Remove unused attribute on Producer's Media Player

--- capture/d2l-capture-producer/d2l-capture-producer-editor.js
+++ capture/d2l-capture-producer/d2l-capture-producer-editor.js
@@ -183,7 +183,6 @@ class CaptureProducerEditor extends RtlMixin(InternalLocalizeMixin(LitElement)) 
 						controls
 						crossorigin="anonymous"
 						@cuechange="${this._handleCueChange}"
-						hideseekbar
 						hide-seek-bar
 						@pause="${this._pauseUpdatingVideoTime}"
 						@play="${this._startUpdatingVideoTime}"
