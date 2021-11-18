44d65d17e1a07da56aff250f5aed321d9c4f149b
Rhys Causey <rhys.causey@D2L.com>
2021-11-18 17:47:33 -0500

    Prevent multiple attempts to load with same urls

--- core/d2l-content-viewer/d2l-content-viewer.js
+++ core/d2l-content-viewer/d2l-content-viewer.js
@@ -45,6 +45,7 @@ class ContentViewer extends LitElement {
 		this._lastTrackLoadFailedTime = null;
 		this._trackErrorFetchTimeoutId = null;
 		this._revision = null;
+		this._refreshExpiry = null;
 	}
 
 	async firstUpdated() {
@@ -63,10 +64,10 @@ class ContentViewer extends LitElement {
 			});
 		}
 
-		await this.loadRevisionData();
-		await this.loadMedia();
-		await this.loadCaptions();
-		await this.loadMetadata();
+		await this._loadRevisionData();
+		await this._loadMedia();
+		await this._loadCaptions();
+		await this._loadMetadata();
 
 		this.dispatchEvent(new CustomEvent('cs-content-loaded', {
 			bubbles: true,
@@ -78,18 +79,18 @@ class ContentViewer extends LitElement {
 		return this._mediaSources && this._mediaSources.length > 0 && html`
 			<d2l-labs-media-player
 				crossorigin="anonymous"
-				@error=${this.onError}
-				@trackloadfailed=${this.onTrackLoadFailed}
-				@tracksmenuitemchanged=${this.onTracksChanged}
+				@error=${this._onError}
+				@trackloadfailed=${this._onTrackLoadFailed}
+				@tracksmenuitemchanged=${this._onTracksChanged}
 				?allow-download=${this.allowDownload}
 				?allow-download-on-error=${this.allowDownloadOnError}>
-				${this._mediaSources.map(mediaSource => this.renderMediaSource(mediaSource))}
-				${this._captionSignedUrls.map(captionSignedUrl => this.renderCaptionsTrack(captionSignedUrl))}
+				${this._mediaSources.map(mediaSource => this._renderMediaSource(mediaSource))}
+				${this._captionSignedUrls.map(captionSignedUrl => this._renderCaptionsTrack(captionSignedUrl))}
 			</d2l-labs-media-player>
 		`;
 	}
 
-	async loadCaptions() {
+	async _loadCaptions() {
 		clearInterval(this._trackErrorFetchTimeoutId);
 		this._trackErrorFetchTimeoutId = null;
 
@@ -110,7 +111,7 @@ class ContentViewer extends LitElement {
 		}
 	}
 
-	async loadMetadata() {
+	async _loadMetadata() {
 		const metadata = this.activity ? await this.hmClient.getMetadata(this._resourceEntity)
 			: await this.client.getMetadata();
 
@@ -119,17 +120,17 @@ class ContentViewer extends LitElement {
 		mediaPlayer.metadata = metadata;
 	}
 
-	async loadMedia() {
+	async _loadMedia() {
 		if (this.activity) {
 			this._mediaSources = await this.hmClient.getMedia(this._resourceEntity);
 		} else if (this.href) {
-			this._mediaSources = [await this.getMediaSource()];
+			this._mediaSources = [await this._getMediaSource()];
 		} else {
-			this._mediaSources = await Promise.all(this._revision.formats.map(format => this.getMediaSource(format)));
+			this._mediaSources = await Promise.all(this._revision.formats.map(format => this._getMediaSource(format)));
 		}
 	}
 
-	async loadRevisionData() {
+	async _loadRevisionData() {
 		if (this.activity) {
 			const revision = await this.hmClient.getRevision(this._resourceEntity);
 			this._revision = {
@@ -144,48 +145,55 @@ class ContentViewer extends LitElement {
 			};
 		}
 
-		this.verifyContentType(this._revision.type);
+		this._verifyContentType(this._revision.type);
 	}
 
-	onError() {
-		if (this._mediaSources && this._mediaSources.some(s => s.expires && (s.expires - Date.now() < 0))) {
-			this.loadMedia();
+	_onError() {
+		if (this._mediaSources && this._mediaSources.length > 0) {
+			const expires = this._mediaSources[0].expires;
+			if (this._refreshExpiry !== expires && expires - Date.now() < 0) {
+				// Prevent multiple attempts to load with same URLs
+				this._refreshExpiry = expires;
+
+				// Get new signed URLs and load them
+				this._loadMedia();
+			}
 		}
 	}
 
-	async onTrackLoadFailed() {
+	async _onTrackLoadFailed() {
 		const elapsedTimeSinceLastTrackLoadFailed = (new Date()).getTime() - (this._lastTrackLoadFailedTime || 0);
 		const trackErrorFetchIntervalElapsed = elapsedTimeSinceLastTrackLoadFailed > TRACK_ERROR_FETCH_INTERVAL_MILLISECONDS;
 
 		if (trackErrorFetchIntervalElapsed && !this._trackErrorFetchTimeoutId) {
 			this._lastTrackLoadFailedTime = (new Date()).getTime();
-			await this.loadCaptions();
+			await this._loadCaptions();
 		} else if (!trackErrorFetchIntervalElapsed && !this._trackErrorFetchTimeoutId) {
 			this._lastTrackLoadFailedTime = (new Date()).getTime();
-			this._trackErrorFetchTimeoutId = setTimeout(this.loadCaptions.bind(this), TRACK_ERROR_FETCH_INTERVAL_MILLISECONDS);
+			this._trackErrorFetchTimeoutId = setTimeout(this._loadCaptions.bind(this), TRACK_ERROR_FETCH_INTERVAL_MILLISECONDS);
 		}
 	}
 
-	async onTracksChanged() {
+	async _onTracksChanged() {
 		const elapsedTimeSinceLoadCaptions = (new Date()).getTime() - this._captionsSignedUrlStartTime;
 		if (elapsedTimeSinceLoadCaptions > this._captionsSignedUrlExpireTime) {
-			await this.loadCaptions();
+			await this._loadCaptions();
 		}
 	}
 
-	async getMediaSource(format) {
+	async _getMediaSource(format) {
 		return this.client.getDownloadUrl({format, href: this.href});
 	}
 
-	renderCaptionsTrack(captionsUrl) {
+	_renderCaptionsTrack(captionsUrl) {
 		return html`<track src="${captionsUrl.Value}" kind="captions" label=${captionsUrl.Locale} srclang=${captionsUrl.Locale.slice(0, 2)}>`;
 	}
 
-	renderMediaSource(source) {
+	_renderMediaSource(source) {
 		return html`<source src=${source.src} label=${source.format} ?default=${source.format === VideoFormat.HD}>`;
 	}
 
-	async verifyContentType(type) {
+	async _verifyContentType(type) {
 		if (!VALID_CONTENT_TYPES.includes(type)) {
 			throw new Error(`type ${type.key} unsupported`);
 		}
