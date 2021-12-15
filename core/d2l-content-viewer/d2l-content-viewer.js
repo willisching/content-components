e8b0d9f6d7aa59f8bd18184e6e3faa3e08e076f1
Michael Kovalchuk <Michael.Kovalchuk@D2L.com>
2021-12-15 16:06:50 -0500

    Revert "Removed on-track-changed listener"

--- core/d2l-content-viewer/d2l-content-viewer.js
+++ core/d2l-content-viewer/d2l-content-viewer.js
@@ -46,6 +46,7 @@ class ContentViewer extends InternalLocalizeMixin(LitElement) {
 		super();
 		this._resourceEntity = null;
 		this._captionSignedUrls = [];
+		this._captionsSignedUrlStartTime = null;
 		this._lastTrackLoadFailedTime = null;
 		this._trackErrorFetchTimeoutId = null;
 		this._revision = null;
@@ -90,6 +91,7 @@ class ContentViewer extends InternalLocalizeMixin(LitElement) {
 				@error=${this._onError}
 				@loadeddata=${this._onLoadedData}
 				@trackloadfailed=${this._onTrackLoadFailed}
+				@tracksmenuitemchanged=${this._onTracksChanged}
 				?allow-download-on-error=${this.allowDownloadOnError}>
 				${this._mediaSources.map(mediaSource => this._renderMediaSource(mediaSource))}
 				${this._captionSignedUrls.map(captionSignedUrl => this._renderCaptionsTrack(captionSignedUrl))}
@@ -144,6 +146,8 @@ class ContentViewer extends InternalLocalizeMixin(LitElement) {
 			await this.updateComplete;
 
 			this._captionSignedUrls = captionSignedUrls;
+			this._captionsSignedUrlStartTime = (new Date()).getTime();
+			this._captionsSignedUrlExpireTime = ((this._captionSignedUrls.length && this._captionSignedUrls[0].ExpireTime) || 0) * 1000;
 			this.requestUpdate();
 		}
 	}
@@ -226,6 +230,12 @@ class ContentViewer extends InternalLocalizeMixin(LitElement) {
 		if (shouldWaitToLoadCaptions) {
 			this._trackErrorFetchTimeoutId = setTimeout(this._loadCaptions.bind(this), TRACK_ERROR_FETCH_WAIT_MILLISECONDS);
 		} else {
+			await this._loadCaptions();
+		}
+	}
+
+	async _onTracksChanged() {
+		if (Date.now() > this._captionsSignedUrlExpireTime) {
 			await this._loadCaptions();
 		}
 	}
