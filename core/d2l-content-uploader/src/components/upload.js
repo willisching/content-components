07776ec50bdccac9a5800fb1c8c63fb6f1ac03ff
Lennard Chan <lchan@c02fg19hmd6q.desire2learn.d2l>
2022-01-31 14:49:31 -0500

    Add message

--- core/d2l-content-uploader/src/components/upload.js
+++ core/d2l-content-uploader/src/components/upload.js
@@ -114,6 +114,7 @@ export class Upload extends RtlMixin(RequesterMixin(InternalLocalizeMixin(LitEle
 						${this.errorMessage ? html`<p id="error-message" class="d2l-body-compact">${this.errorMessage}&nbsp;</p>` : ''}
 					</div>
 				</file-drop>
+				<p>You can upload files up to a maximum of ${formatFileSize(this.maxFileSizeInBytes)}</p>
 			</div>
 		`;
 	}
