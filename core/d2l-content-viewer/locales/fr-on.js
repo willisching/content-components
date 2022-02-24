dbd337165a94162da35bd26d5097415655b98754
JenkinsReader <resupport@desire2learn.com>
2022-02-24 16:34:36 -0500

    Translation Updates

--- core/d2l-content-viewer/locales/fr-on.js
+++ core/d2l-content-viewer/locales/fr-on.js
@@ -1,7 +1,9 @@
 export const val = {
+	deletedMedia: 'This media file has been deleted',
 	download: 'Télécharger',
 	formatHD: 'HD',
 	formatLD: 'LD',
 	formatMP3: 'MP3',
 	formatSD: 'SD',
+	mediaFileIsProcessing: 'This media file is currently being processed. Please come back later.'
 };
