f94ed71fba53877b7823bc9b25256fc025858dee
Mark Padilla <markjjpadilla@gmail.com>
2022-03-01 14:30:48 -0500

    feat: Add Launch button and enhance UI in "Capture Encoder" page (#232)
    US135510

--- applications/d2l-capture-central/src/pages/encoder/d2l-capture-central-encoder.js
+++ applications/d2l-capture-central/src/pages/encoder/d2l-capture-central-encoder.js
@@ -1,9 +1,12 @@
-import '@brightspace-ui/core/components/icons/icon-custom.js';
+import '@brightspace-ui/core/components/alert/alert-toast.js';
+import '@brightspace-ui/core/components/button/button.js';
 import '@brightspace-ui/core/components/loading-spinner/loading-spinner.js';
 
 import { css, html } from 'lit-element/lit-element.js';
 import { DependencyRequester } from '../../mixins/dependency-requester-mixin.js';
+import { heading2Styles, bodyStandardStyles } from '@brightspace-ui/core/components/typography/styles.js';
 import { InternalLocalizeMixin } from '../../mixins/internal-localize-mixin';
+import isMobile from 'ismobilejs';
 import { PageViewElement } from '../../components/page-view-element';
 
 class D2LCaptureCentralEncoder extends InternalLocalizeMixin(DependencyRequester(PageViewElement)) {
@@ -14,7 +17,7 @@ class D2LCaptureCentralEncoder extends InternalLocalizeMixin(DependencyRequester
 	}
 
 	static get styles() {
-		return css`
+		return [bodyStandardStyles, heading2Styles, css`
 			#d2l-capture-central-encoder-page {
 				margin-top: 30px;
 				width: 100%;
@@ -26,52 +29,83 @@ class D2LCaptureCentralEncoder extends InternalLocalizeMixin(DependencyRequester
 				width: 100%;
 			}
 
-			#d2l-capture-central-encoder-downloads-container {
-				display: flex;
-				flex-direction: column;
-				margin-left: 30px;
-			}
-
-			.d2l-capture-central-encoder-download-link-container {
-				align-items: center;
+			#d2l-capture-central-encoder-buttons-container {
 				display: flex;
 				flex-direction: row;
-				margin-left: 25px;
-				margin-bottom: 25px;
+				margin-top: 35px;
 			}
 
-			.d2l-capture-central-encoder-download-link {
-				margin-left: 10px;
+			.d2l-capture-central-encoder-button {
+				margin-right: 10px;
 			}
-		`;
+		`];
 	}
 
 	async connectedCallback() {
 		super.connectedCallback();
+
 		this.captureServiceClient = this.requestDependency('capture-service-client');
-		this._encoderDownloads = await this.captureServiceClient.getEncoderUpdates();
-		if (this._encoderDownloads) {
-			this._reloadEncoderDownloadsOnExpiry();
+
+		if (isMobile(window.navigator).any) {
+			this._userPlatform = 'mobile';
+		} else if (navigator.userAgent.includes('Macintosh')) {
+			this._userPlatform = 'mac';
+		} else if (navigator.userAgent.includes('Windows')) {
+			this._userPlatform = 'win';
+		} else {
+			this._userPlatform = 'unknown';
+		}
+
+		if (this._userPlatform !== 'mobile') {
+			await this._loadEncoderDownloads();
+			if (this._encoderDownloads && this._encoderDownloads.length > 0) {
+				this._reloadEncoderDownloadsOnExpiry();
+			}
 		}
 	}
 
 	render() {
 		let content;
-		if (!this._encoderDownloads) {
+		if (!this._encoderDownloads && this._userPlatform !== 'mobile') {
 			content = html`
 				<div id='d2l-capture-central-encoder-loading-container'>
 					<d2l-loading-spinner size="100"></d2l-loading-spinner>
 				</div>
 			`;
-		} else if (this._encoderDownloads.length === 0) {
+		} else if (this._encoderDownloads && this._encoderDownloads.length === 0) {
 			content = html`
 				<p class="d2l-body-standard">${this.localize('noCaptureEncoderDownloadsAvailable')}</p>
 			`;
+		} else if (this._userPlatform === 'mobile') {
+			return html`
+				<div id='d2l-capture-central-encoder-downloads-container'>
+					<h2 class="d2l-heading-2">${this.localize('captureEncoder')}</h2>
+					<p class="d2l-body-standard">${this.localize('captureEncoderDescription')}</p>
+					<p class="d2l-body-standard"><b>${this.localize('captureEncoderDesktopOnly')}</b></p>
+				</div>
+			`;
 		} else {
+			const instructionsText = this._userPlatform === 'unknown' ? html`
+				<p class="d2l-body-standard"><b>${this.localize('captureEncoderUnsupportedPlatform')}</b></p>
+			` : html`
+				<p class="d2l-body-standard">${this.localize('captureEncoderLaunchInstructions')}</p>
+			`;
 			content = html`
 				<div id='d2l-capture-central-encoder-downloads-container'></div>
-					<p class="d2l-body-standard">${this.localize('downloadCaptureEncoder')}</p>
-					${this._encoderDownloads.map(encoderDownload => this._renderEncoderDownloadLink(encoderDownload))}
+					<h2 class="d2l-heading-2">${this.localize('captureEncoder')}</h2>
+					<p class="d2l-body-standard">${this.localize('captureEncoderDescription')}</p>
+					${instructionsText}
+					<div id="d2l-capture-central-encoder-buttons-container">
+						<d2l-button
+							class="d2l-capture-central-encoder-button"
+							@click="${this._launchEncoder}"
+							?disabled="${this._userPlatform === 'unknown'}"
+							primary
+						>
+							${this.localize('launch')}
+						</d2l-button>
+						${this._encoderDownloads.map(encoderDownload => this._renderEncoderDownloadButton(encoderDownload))}
+					</div>
 				</div>
 			`;
 		}
@@ -83,35 +117,32 @@ class D2LCaptureCentralEncoder extends InternalLocalizeMixin(DependencyRequester
 		`;
 	}
 
-	_getPlatformLinkDetails(shortPlatformName) {
-		switch (shortPlatformName.toLowerCase()) {
+	_getEncoderDownloadText(platform) {
+		switch (platform.toLowerCase()) {
 			case 'mac':
-				return {
-					langterm: 'mac',
-					// https://commons.wikimedia.org/wiki/File:Apple_logo_black.svg
-					icon: html`
-						<svg xmlns="http://www.w3.org/2000/svg" width="814" height="1000" viewBox="0 0 814 1000" xml:space="preserve">
-							<path d="M788.1 340.9c-5.8 4.5-108.2 62.2-108.2 190.5 0 148.4 130.3 200.9 134.2 202.2-.6 3.2-20.7 71.9-68.7 141.9-42.8 61.6-87.5 123.1-155.5 123.1s-85.5-39.5-164-39.5c-76.5 0-103.7 40.8-165.9 40.8s-105.6-57-155.5-127C46.7 790.7 0 663 0 541.8c0-194.4 126.4-297.5 250.8-297.5 66.1 0 121.2 43.4 162.7 43.4 39.5 0 101.1-46 176.3-46 28.5 0 130.9 2.6 198.3 99.2zm-234-181.5c31.1-36.9 53.1-88.1 53.1-139.3 0-7.1-.6-14.3-1.9-20.1-50.6 1.9-110.8 33.7-147.1 75.8-28.5 32.4-55.1 83.6-55.1 135.5 0 7.8 1.3 15.6 1.9 18.1 3.2.6 8.4 1.3 13.6 1.3 45.4 0 102.5-30.4 135.5-71.3z"/>
-						</svg>
-					`
-				};
+				return this.localize('downloadForMac');
 			case 'win':
-				return {
-					langterm: 'windows',
-					// https://commons.wikimedia.org/wiki/File:Windows_logo_-_2021.svg
-					icon: html`
-						<svg xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" width="58" height="58" version="1.1" viewBox="0 0 48.745 48.747">
-							<g fill="#0078d4">
-								<rect x="2.2848e-15" y="-.00011033" width="23.105" height="23.105"/>
-								<rect x="25.64" y="-.00011033" width="23.105" height="23.105"/>
-								<rect x="2.2848e-15" y="25.642" width="23.105" height="23.105"/>
-								<rect x="25.64" y="25.642" width="23.105" height="23.105"/>
-							</g>
-						</svg>
-					`
-				};
+				return this.localize('downloadForWindows');
 			default:
-				throw new Error(`Invalid platform name: ${shortPlatformName}`);
+				throw new Error(`Invalid platform name: ${platform}`);
+		}
+	}
+
+	_launchEncoder() {
+		window.location.href = 'd2lce://launch';
+	}
+
+	async _loadEncoderDownloads() {
+		this._encoderDownloads = await this.captureServiceClient.getEncoderUpdates();
+
+		if (this._encoderDownloads && this._encoderDownloads.length > 0) {
+			const downloadMatchingUserPlatform = this._encoderDownloads.find(encoderDownload => encoderDownload.platform === this._userPlatform);
+			if (downloadMatchingUserPlatform && this._encoderDownloads[0].url !== downloadMatchingUserPlatform.url) {
+				// Move the download matching the user's platform to the start of the list so that it shows up first on the page.
+				const reorderedEncoderDownloads = this._encoderDownloads.filter(encoderDownload => encoderDownload.url !== downloadMatchingUserPlatform.url);
+				reorderedEncoderDownloads.unshift(downloadMatchingUserPlatform);
+				this._encoderDownloads = reorderedEncoderDownloads;
+			}
 		}
 	}
 
@@ -128,9 +159,8 @@ class D2LCaptureCentralEncoder extends InternalLocalizeMixin(DependencyRequester
 		const timeUntilExpiry = (earliestExpiryTimeSeconds * 1000) - Date.now();
 
 		setTimeout(() => {
-			this.captureServiceClient.getEncoderUpdates()
-				.then(encoderDownloads => {
-					this._encoderDownloads = encoderDownloads;
+			this._loadEncoderDownloads()
+				.then(() => {
 					this._reloadEncoderDownloadsOnExpiry();
 				});
 		// The HTTP request can take some time, so we load the new URLs slightly earlier than
@@ -138,17 +168,24 @@ class D2LCaptureCentralEncoder extends InternalLocalizeMixin(DependencyRequester
 		}, timeUntilExpiry - 10000);
 	}
 
-	_renderEncoderDownloadLink(encoderDownload) {
-		const linkDetails = this._getPlatformLinkDetails(encoderDownload.platform);
+	_renderEncoderDownloadButton(encoderDownload) {
+		const downloadText = this._getEncoderDownloadText(encoderDownload.platform);
+		const downloadEncoderFromUrl = () => {
+			// We use this approach to force the browser to open a download prompt without opening a new tab.
+			const tempDownloadAnchor = document.createElement('a');
+			tempDownloadAnchor.href = encoderDownload.url;
+			tempDownloadAnchor.target = '_self';
+			tempDownloadAnchor.download = encoderDownload.url;
+			tempDownloadAnchor.click();
+		};
 		return html`
-			<div class="d2l-capture-central-encoder-download-link-container">
-				<d2l-icon-custom
-					size="tier3"
-				>
-					${linkDetails.icon}
-				</d2l-icon-custom>
-				<div class="d2l-capture-central-encoder-download-link d2l-body-standard"><a href="${encoderDownload.url}">${this.localize(linkDetails.langterm)}</a></div>
-			</div>
+			<d2l-button
+				class="d2l-capture-central-encoder-button"
+				@click="${downloadEncoderFromUrl}"
+				size="tier3"
+			>
+				${downloadText}
+			</d2l-button>
 		`;
 	}
 }
