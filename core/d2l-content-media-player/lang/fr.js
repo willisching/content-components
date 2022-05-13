94e299ba002fef80a27de744fff98be885f8204c
Rhys Causey <rhys.causey@D2L.com>
2022-05-13 11:39:45 -0400

    Langterm updates for new component

--- core/d2l-content-media-player/lang/fr.js
+++ core/d2l-content-media-player/lang/fr.js
@@ -1,7 +1,11 @@
 export const val = {
+	deletedMedia: 'Ce fichier multimédia a été supprimé',
 	download: 'Télécharger',
 	formatHD: 'HD',
 	formatLD: 'LD',
 	formatMP3: 'MP3',
 	formatSD: 'SD',
+	formatSource: 'Source',
+	mediaFileIsProcessing: 'Ce fichier multimédia est en cours de traitement. Revenez plus tard.',
+	mediaFileIsProcessingInserting: 'Votre fichier multimédia est en cours de traitement. Veuillez patienter, enregistrer ou revenir plus tard. Vous recevrez une notification lorsque le traitement sera terminé.'
 };
---
+++ core/d2l-content-media-player/lang/hi.js
@@ -0,0 +1,11 @@
+export const val = {
+	deletedMedia: 'यह मीडिया फ़ाइल हटा दी गई है',
+	download: 'डाउनलोड करें',
+	formatHD: 'HD',
+	formatLD: 'LD',
+	formatMP3: 'MP3',
+	formatSD: 'SD',
+	formatSource: 'स्रोत',
+	mediaFileIsProcessing: 'इस मीडिया फ़ाइल को अभी संसाधित कर रहे हैं। कृपया बाद में फिर देखें।',
+	mediaFileIsProcessingInserting: 'आपकी मीडिया फ़ाइल अब संसाधित की जा रही है। कृपया प्रतीक्षा करें या सेव करें और बाद में वापस आएँ। संसाधन पूर्ण होने पर आपको सूचना मिलेगी।'
+};
