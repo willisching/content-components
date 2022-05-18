984b68d983f92195646cbc8bef3f61e78d860b3d
Rhys Causey <rhys.causey@d2l.com>
2022-05-18 16:32:07 -0400

    Revert "Revert "d2l content scorm player""

--- core/d2l-content-renderer/lang/en.js
+++ core/d2l-content-renderer/lang/en.js
@@ -1,6 +1,8 @@
-/* eslint quotes: 0 */
-
 export default {
-	"unsupportedType": "This content cannot be rendered as it is unsupported.",
-	"errorRenderingContent": "Error when trying to load this content."
+	unsupportedType: 'This content cannot be rendered as it is unsupported.',
+	errorRenderingContent: 'Error when trying to load this content.',
+	deletedMedia: 'This media file has been deleted',
+	generalErrorMessage: 'An error occured while trying to load this resource.',
+	mediaFileIsProcessing: 'This media file is currently being processed. Please come back later.',
+	revisionProcessingFailedMessage: 'An error occured while processing the video. Please try again. Ensure that uploaded files are in a supported format.',
 };
