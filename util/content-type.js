94fa02318c87845ea2425f7e39a4614c477f9497
Quan Hui Wang <qwang@desire2learn.com>
2022-10-19 16:02:48 -0400

    Add other type definitions with extensions.

--- util/content-type.js
+++ util/content-type.js
@@ -1,5 +1,14 @@
 export default Object.freeze({
 	SCORM: 'Scorm',
 	VIDEO: 'Video',
-	AUDIO: 'Audio'
+	AUDIO: 'Audio',
+	DOC: 'Doc',
+	DOCX: 'Docx',
+	JPG: 'Jpg',
+	PDF: 'Pdf',
+	PNG: 'Png',
+	PPT: 'Ppt',
+	PPTX: 'Pptx',
+	XLS: 'Xls',
+	XLSX: 'Xlsx'
 });
